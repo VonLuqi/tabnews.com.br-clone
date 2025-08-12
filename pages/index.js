c58db4968d50edb45c51c471ef2342443dd3429f
VonLuqi <theworldofaiz@gmail.com>
2025-08-12 17:51:33 +0000

    modifica a frase

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,6 @@
 function Home()
 {
-    return <h1>Tacielle, eu te amo. Se me ama também dá uma risadinha kkk.</h1>
+    return <h1>Tacielle, eu te amo. Se me ama também, dá uma risadinha kkk. ❤️</h1>
 }
 
 export default Home;
